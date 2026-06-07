8fc8671c4ea6f56cfdf0ad75ca167a92002e97d2
luis-hdz7 <luisjorgeantonhernandez@gmail.com>
2026-06-07 14:37:48 -0600

    cambiando los permisos de las rutas

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,14 +1,18 @@
 import express from 'express';
+import {verifyToken} from "../middleware/authMiddleware.js"
+import authorizeRoles from "../middleware/roleMiddleware.js"
 import { getAllProducts, createProduct,getProduct, updateProduct, deleteProduct } from "../controllers/productControllers.js"
 
 const router = express.Router();
 
 //* Todos pueden acceder a esta ruta (users, employee, admin)
 router.get("/",getAllProducts)
-router.get("/:id",getProduct)
-router.post("/",createProduct)
-router.put("/:id",updateProduct)
+//* EMPLOYEE & ADMIN pueden acceder a esta ruta
+router.get("/:id",verifyToken, authorizeRoles("EMPLOYEE", "ADMIN"),getProduct)
+//* EMPLOYEE & ADMIN pueden acceder a esta ruta
+router.post("/",verifyToken, authorizeRoles("EMPLOYEE", "ADMIN"),createProduct)
+//* EMPLOYEE & ADMIN pueden acceder a esta ruta
+router.put("/:id",verifyToken, authorizeRoles("EMPLOYEE", "ADMIN"),updateProduct)
 //*Solo ADMIN puede acceder a esta ruta
-router.delete("/:id",deleteProduct)
-
+router.delete("/:id",verifyToken, authorizeRoles("ADMIN"),deleteProduct)
 export default router;
